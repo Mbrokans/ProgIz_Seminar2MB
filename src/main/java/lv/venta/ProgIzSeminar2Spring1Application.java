c7f42c5006b130221f8fcc8c21e83549e959ce0f
Mbrokans <s25brokmart@venta.lv>
2026-05-20 15:47:39 +0300

    EDIT: start

---
+++ src/main/java/lv/venta/ProgIzSeminar2Spring1Application.java
@@ -0,0 +1,13 @@
+package lv.venta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgIzSeminar2Spring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgIzSeminar2Spring1Application.class, args);
+	}
+
+}
---
+++ src/test/java/lv/venta/ProgIzSeminar2Spring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.venta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgIzSeminar2Spring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
