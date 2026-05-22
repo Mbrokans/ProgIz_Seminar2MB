0eb397aa6ccf8d11520aad2810fc1c21fc9b990e
Mbrokans <s25brokmart@venta.lv>
2026-05-22 17:41:33 +0300

    EDIT:joined grade and student classes

---
+++ src/main/java/lv/venta/model/Grade.java
@@ -0,0 +1,40 @@
+package lv.venta.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Table(name="GradeTable")
+@Entity
+public class Grade {
+	@Column(name="Idg")
+	@Id
+	@GeneratedValue (strategy= GenerationType.AUTO)
+	@Setter(value = AccessLevel.NONE)
+	private long idg;
+	@Column(name = "grade")
+	@Min(0)
+	@Max(10)
+	private int grade;
+	@ManyToOne
+	@JoinColumn(name="Ids")
+	private Student student;
+	private Course course;
+	private Professors professor;
+}
