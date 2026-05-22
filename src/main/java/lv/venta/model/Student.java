94d4f7a51411159c95fe8f06dc0c8a71f6f0770c
Mbrokans <s25brokmart@venta.lv>
2026-05-22 16:48:39 +0300

    EDIT: finished student class

--- src/main/java/lv/venta/model/Student.java
+++ src/main/java/lv/venta/model/Student.java
@@ -1,5 +1,14 @@
 package lv.venta.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,12 +20,24 @@ import lombok.ToString;
 @Setter
 @NoArgsConstructor
 @ToString
+@Table(name="StudentTable")
+@Entity
 public class Student {
 	
+	@Column(name="Ids")
+	@Id
+	@GeneratedValue (strategy= GenerationType.AUTO)
 	@Setter(value = AccessLevel.NONE)
 	private long ids;
-	
+	@Column(name = "Name")
+	@NotEmpty
+	@NotNull
+	@Pattern(regexp="[A-Z]{1}[a-z]{40}([ ]{1}([A-Z]{1}[a-z]{2,40}))?")
 	private String name;
+	@Column(name = "Surname")
+	@NotEmpty
+	@NotNull
+	@Pattern(regexp="[A-Z]{1}[a-z]{40}([ -]{1}([A-Z]{1}[a-z]{2,40}))?")
 	private String surname;
 	
 	public Student(String name, String surname) {
