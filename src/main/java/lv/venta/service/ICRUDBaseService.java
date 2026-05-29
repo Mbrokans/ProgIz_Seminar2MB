db4aff1015980995428ec87bee922ea0caf5e127
Mbrokans <s25brokmart@venta.lv>
2026-05-29 17:33:43 +0300

    EDIT: added a anotjer function

--- src/main/java/lv/venta/service/ICRUDBaseService.java
+++ src/main/java/lv/venta/service/ICRUDBaseService.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 
 public interface ICRUDBaseService<Ttype> {
 	//retrieve all, retrieve by id
+	
 	public abstract ArrayList<Ttype> retrieveAll() throws Exception;
 	
 	public abstract Ttype retrieveById(long id)throws Exception;
 	
 	public abstract void deleteById(long id) throws Exception;
+	
+	public abstract void create(Ttype newObject) throws Exception;
 }
---
+++ src/main/java/lv/venta/service/ICRUDStudentService.java
@@ -0,0 +1,7 @@
+package lv.venta.service;
+
+import lv.venta.model.Student;
+
+public interface ICRUDStudentService extends ICRUDBaseService<Student> {
+	public abstract void updateById(long id, String name, String surname)throws Exception;
+}
