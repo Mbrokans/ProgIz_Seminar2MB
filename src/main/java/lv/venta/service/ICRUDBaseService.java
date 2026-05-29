4b9134bab24355c028e1100de9b55ff586dbf56c
Mbrokans <s25brokmart@venta.lv>
2026-05-29 17:25:58 +0300

    EDIT: added a crudbase service

---
+++ src/main/java/lv/venta/service/ICRUDBaseService.java
@@ -0,0 +1,12 @@
+package lv.venta.service;
+
+import java.util.ArrayList;
+
+public interface ICRUDBaseService<Ttype> {
+	//retrieve all, retrieve by id
+	public abstract ArrayList<Ttype> retrieveAll() throws Exception;
+	
+	public abstract Ttype retrieveById(long id)throws Exception;
+	
+	public abstract void deleteById(long id) throws Exception;
+}
